c1268e8d70df8a6c7f61a30fcb20ce510883d3ce
roberto <rarimol@ieszaidinvergeles.org>
2025-10-06 12:11:35 +0200

    comit 1

---
+++ Ejercicio2.py
@@ -0,0 +1,6 @@
+Capital=float(input("que cantidad deseas invertir"))
+TasaInteres=0.1
+Tiempo=1
+
+total=(Capital*TasaInteres)*Tiempo
+print("Tu capital obtenido de la inversion es ",total)
---
+++ prueba.py
@@ -0,0 +1,6 @@
+x=float(input("Introduzca su peso"))
+y=float(input("Introduzca su estatura"))
+
+#indice de masa corporal#
+total = x / (y ** 2)
+print("tu Indice de masa corporal es ", total)
