cbb0474a83bda66a0515e8c792332d4c7010a5ea
roberto <rarimol@ieszaidinvergeles.org>
2025-10-09 10:32:26 +0200

    Ejercicio6

--- Ejer6.py
+++ Ejer6.py
@@ -2,4 +2,7 @@
 #después muestre en pantalla la suma de todos los enteros desde 1 hasta . La suma
 #de los primeros enteros positivos puede ser calculada de la siguiente forma:
 
+leer=int(input("Introduzca un numero entero positivo"))
 
+suma=leer*(leer+1)/2
+print("La  suma es",suma)
