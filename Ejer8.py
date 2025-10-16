dbe506801aa538a638e8fb1b6fef951874b1dedc
roberto <rarimol@ieszaidinvergeles.org>
2025-10-16 09:39:08 +0200

    Subir

--- Ejer8.py
+++ Ejer8.py
@@ -3,6 +3,10 @@
 #<n> y <m> son los números introducidos por el usuario, y <c> y <r> son el cociente
 #y el resto de la división entera respectivamente.
 
-num1=int(input("Introduce un numero"))
-num2=int(input("Introduce un numero"))
+n1=int(input("Introduce un numero entero(n)"))
+n2=int(input("Introduce un numero entero(n)"))
 
+c=n1//n2
+r=n1%n2
+
+print(n1,"entre",n2,"Da un cociente de ",c,"y un resto de ",r)
