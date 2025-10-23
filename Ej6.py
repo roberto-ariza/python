04ab555ebcc3044180bcc5f9cf5049e6865ec0ca
roberto <rarimol@ieszaidinvergeles.org>
2025-10-23 10:25:11 +0200

    Ejercicio replace

---
+++ Ej6.py
@@ -0,0 +1,11 @@
+#Escribir un programa que pida al usuario que 
+# introduzca una frase en la consola y
+#una vocal, y después muestre por pantalla la 
+# misma frase pero con la vocal
+#introducida en mayúscula.
+frase=input("introduzca una frase : ") 
+vocal=input("introduzca una vocal")
+x=frase.replace(vocal,vocal.upper())
+print(x)
+
+
