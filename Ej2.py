f0d95b54dd01c0fe5b79e571639f7e63f3226a79
roberto <rarimol@ieszaidinvergeles.org>
2025-10-23 09:45:25 +0200

    Actualizacion ej3

--- Ej2.py
+++ Ej2.py
@@ -10,3 +10,4 @@ nombre = input("Introduce tu nombre completo: ")
 print(nombre.lower())
 print(nombre.upper())
 print(nombre.title())
+
---
+++ Ej3.py
@@ -0,0 +1,8 @@
+#Escribir un programa que pregunte el nombre del usuario en la consola y después
+#de que el usuario lo introduzca muestre por pantalla <NOMBRE> tiene <n>
+#letras, donde <NOMBRE> es el nombre de usuario en mayúsculas y <n> es el
+#número de letras que tienen el nombre.
+
+nombre=input("Introduzca el nombre de usuario :")
+
+print("Hola "+nombre)
