0a785cdf0ded08e6cc4e53fa2991d319d77364f9
roberto <rarimol@ieszaidinvergeles.org>
2025-10-23 10:59:56 +0200

    new

--- Ej8.py
+++ Ej8.py
@@ -3,3 +3,9 @@
 #con dos decimales y muestre por pantalla el 
 # número de euros y el número de
 #céntimos del precio introducido.
+precio=str(input("Indique precio de un producto : "))
+new=precio.split(".")
+euros=new[0]
+cents=new[1]
+print("El producto  vale",euros,"en €")
+print("El producto vale",cents,"em cnts") 
