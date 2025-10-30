21c982ef91d779feccffdcf8daa98d6c77710f9c
roberto <rarimol@ieszaidinvergeles.org>
2025-10-30 10:06:38 +0100

    Ejercicio 1,2,3

---
+++ EjerciciosCondicionales/Ej1.py
@@ -0,0 +1,9 @@
+#Escribir un programa que pregunte al usuario su edad y muestre por pantalla si es
+#mayor de edad o no.
+edad=int(input("Cual es  tu edad? : "))
+
+if edad>=18:
+    print("Eres mayor de edad")
+else:
+    print("no eres mayor de edad")
+
