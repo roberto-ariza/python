892ca02e58add6cf6ab3231492bb85079cf54af8
roberto <rarimol@ieszaidinvergeles.org>
2025-10-30 10:18:01 +0100

    Ejercicio 4

---
+++ EjerciciosCondicionales/Ej4.py
@@ -0,0 +1,8 @@
+#Escribir un programa que pida al usuario un número entero y muestre por pantalla si
+#es par o impar
+
+num1=int(input("Introduzca un numero"))
+if num1%2==0:
+    print("Es par")
+else:
+    print("No es par")
