d23816361216d0e3bbe4b4361a2ded2e7d7ecd17
roberto <rarimol@ieszaidinvergeles.org>
2025-11-06 09:38:50 +0100

    Ejercicio 2 bucles

---
+++ Bucles/Ej1.py
@@ -0,0 +1,7 @@
+#Ejercicio 1
+#Escribir un programa que pida al usuario una palabra y la muestre por pantalla 10
+#veces.
+
+palabra=input("Dime una palabra")
+for i in range(10):
+    print(palabra)
