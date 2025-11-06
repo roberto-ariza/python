918032e829d27a8b8c83f45215c65aca74532a1c
roberto <rarimol@ieszaidinvergeles.org>
2025-11-06 10:07:33 +0100

    ejercicio 4 bucles

---
+++ Bucles/Ej4.py
@@ -0,0 +1,7 @@
+#Ejercicio 4
+#Escribir un programa que pida al usuario un número entero positivo y muestre por
+#pantalla la cuenta atrás desde ese número hasta cero separados por comas.
+
+user=int(input("Numero entero positivo : "))
+for i in range(10,0,-1):
+    print(i)
