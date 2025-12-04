df92d778d957e43471e643b43ea6b270906c6209
roberto <rarimol@ieszaidinvergeles.org>
2025-12-04 10:07:44 +0100

    Ejercicio lista2

---
+++ Listas/Ej2.py
@@ -0,0 +1,9 @@
+#Escribir un programa que almacene las asignaturas de un curso (por ejemplo
+#Matemáticas, Física, Química, Historia y Lengua) en una lista y la muestre por
+#pantalla el mensaje Yo estudio <asignatura>, donde <asignatura> es cada
+#una de las asignaturas de la lista.
+
+asignatura=["Matematicas","Fisica","Historia","Lengua"]
+
+for i in range(len(asignatura)):
+    print("Yo estudio", asignatura[i])
