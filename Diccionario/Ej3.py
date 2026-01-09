a753809d8453a2cbc194979b94da170a9153d26d
roberto <rarimol@ieszaidinvergeles.org>
2026-01-09 14:27:08 +0100

    Ejercicio 3 fruta

--- Diccionario/Ej3.py
+++ Diccionario/Ej3.py
@@ -3,3 +3,13 @@
 #precio de ese número de kilos de fruta. Si la fruta no está en el diccionario debe
 #mostrar un mensaje informando de ello.
 
+diccionario={'platano':1.35,'manzana':0.80,'pera':0.85,'naranja':0.70}
+
+f=input("Que fruta desea?: ")
+k=float(input("Cuanto kg desea?: "))
+
+if f in diccionario:
+    resultado=diccionario[f]*k
+    print(f"El precio de {f} es de ",resultado, "€ en total")
+else:
+    print(f"Esa fruta no existe o no la tenemos")
